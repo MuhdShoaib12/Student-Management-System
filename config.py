757c7b25afc1a9570ac73790dd90b00725318d13
MuhdShoaib12 <shoaibshah9523@gmail.com>
2026-08-17 16:22:40 +0530

    Add config file

---
+++ config.py
@@ -0,0 +1,19 @@
+CSV_FILE = "student.csv"
+JSON_FILE = "students_report.json"
+
+
+SUBJECTS = ["Physics", "Chemistry", "Maths", "Computer", "English"]
+
+MAX_MARKS = 100
+MIN_MARKS = 0
+PASSING_MARKS = 33
+
+GRADE_THRESHOLDS = {
+    "A+": 90,
+    "A": 80,
+    "B": 70,
+    "C": 60,
+    "D": 50,
+    "E": 33,
+    #BELOW THIS FAIL
+}
